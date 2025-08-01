26b487d7b26e6a84210d9a7eb5d4a47fcd0b7ede
vuduong2005 <duongtrieuvu19082005@gmail.comm>
2025-08-02 01:37:20 +0700

    index

--- models/language.js
+++ models/language.js
@@ -1,9 +1,18 @@
-
 const mongoose = require('mongoose');
 
-const LanguageSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  code: { type: String, required: true }
+const languageSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  code: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    default: '',
+  }
 });
 
-module.exports = mongoose.model('Language', LanguageSchema);
+module.exports = mongoose.model('Language', languageSchema);
